51e6ddbe6dbfba8f5e85744de75ca199726389cf
boodieroneil@gmail.com <boodieroneil@gmail.com>
2018-08-30 01:45:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(day, holiday) {
-  return `It's ${day} days until ${holiday}!`
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
 }
